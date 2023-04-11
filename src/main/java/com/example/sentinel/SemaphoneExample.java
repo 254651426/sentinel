65becc5581147ea641448762cbd30d5d1a9bfc6e
254651426 <18971477097@163.com>
2023-04-11 14:46:07 +0800

    新增一个new
    Signed-off-by: 254651426 <18971477097@163.com>

--- src/main/java/com/example/sentinel/SemaphoneExample.java
+++ src/main/java/com/example/sentinel/SemaphoneExample.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Semaphore;
 
 public class SemaphoneExample {
 
-    //Semaphore 小列子 测试 add
+    //Semaphore 小列子 测试 add   new
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
         Semaphore sm = new Semaphore(5);
