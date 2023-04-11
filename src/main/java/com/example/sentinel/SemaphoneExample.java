a561bba62f3df882ee0c0410468ee82286069cdd
254651426 <18971477097@163.com>
2023-04-11 10:45:26 +0800

    修改小东西
    Signed-off-by: 254651426 <18971477097@163.com>

--- src/main/java/com/example/sentinel/SemaphoneExample.java
+++ src/main/java/com/example/sentinel/SemaphoneExample.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Semaphore;
 
 public class SemaphoneExample {
 
-    //Semaphore 小列子  nihao wo shi yj
+    //Semaphore 小列子  nihao wo shi yj 你是谁
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
         Semaphore sm = new Semaphore(5);
